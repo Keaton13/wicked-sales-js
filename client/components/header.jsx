cbc5b66f126edc09708ab1b2d1277f28f9b0a61b
Keaton13 <bajabuggy313@gmail.com>
2021-08-03 10:33:27 -0700

    Added shopping cart icon and fixed header styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 function Header() {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col bg-dark text-white">
-          <h4>$WickedSales</h4>
+      <div className="row maxHeightHeader">
+        <div className="col-10 bg-dark text-white maxHeightHeader">
+          <h4 className="mt-2">$WickedSales</h4>
+        </div>
+        <div className="col-2 bg-dark text-white maxHeightHeader">
+          <img src="../images/download.png" className="headerImageHeight mt-1" />
         </div>
       </div>
     </div>
